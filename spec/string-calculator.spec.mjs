c28bc661d5c00627a1f0e9af27124b4452d15adc
Blanca Bigeriego <blancabcbt@gmail.com>
2024-04-23 21:09:09 +0200

    feat: add function working if receives 1 as an argument

--- spec/string-calculator.spec.mjs
+++ spec/string-calculator.spec.mjs
@@ -1,9 +1,15 @@
 import { StringCalculator } from "../string-calculator.mjs";
 
 describe('StringCalculator', () => {
-    it('should create', () => {
+    it('should return a 0 if an empty string is passed as an argument', () => {
         const stringCalculator = new StringCalculator();
         const result = stringCalculator.add('')
         expect(result).toBe(0)
-    })
+    });
+    it('should return the first string if only one argument is passed', () => {
+        const stringCalculator = new StringCalculator();
+        const result = stringCalculator.add(1)
+        expect(result).toBe(1)
+    });
+    
 })
--- string-calculator.mjs
+++ string-calculator.mjs
@@ -2,5 +2,8 @@ export class StringCalculator {
     add(string1, string2= 0){
         if(string1 === '')
         return 0;
+        if(string1 !== null) 
+        return string1
+    
     }
 }
