2c745c0313ecfa2feb95874b1ea4c2c3a8ca4bf1
Blanca Bigeriego <blancabcbt@gmail.com>
2024-04-23 21:33:31 +0200

    feat: add function working with 4 arguments

--- spec/string-calculator.spec.mjs
+++ spec/string-calculator.spec.mjs
@@ -8,8 +8,23 @@ describe('StringCalculator', () => {
     });
     it('should return the first string if only one argument is passed', () => {
         const stringCalculator = new StringCalculator();
-        const result = stringCalculator.add(1)
+        const result = stringCalculator.add('1')
         expect(result).toBe(1)
     });
-    
+    it('should add the strings if 2 strings passed as an argument', () =>{
+        const stringCalculator = new StringCalculator();
+        const result = stringCalculator.add('1', '1')
+        expect(result).toBe(2)
+    });
+    it('should add 3 numbers', () => {
+        const stringCalculator = new StringCalculator();
+        const result = stringCalculator.add('1', '1', '2')
+        expect(result).toBe(4)
+    });
+    it('should add 4 numbers', () => {
+        const stringCalculator = new StringCalculator();
+        const result = stringCalculator.add('1', '1', '2', '2')
+        expect(result).toBe(6)
+    })
+
 })
--- string-calculator.mjs
+++ string-calculator.mjs
@@ -1,9 +1,9 @@
 export class StringCalculator {
-    add(string1, string2= 0){
+    add(string1, string2 = '0', string3 = '0', string4 = 0){
         if(string1 === '')
         return 0;
-        if(string1 !== null) 
-        return string1
-    
+        if(string1 !== null ) 
+        return parseInt(string1) + parseInt(string2) + parseInt(string3) + parseInt(string4)
+        
     }
 }
